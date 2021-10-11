237bbdb1274d726426b5894465dd290b3474e2a0
Ulzahk <ulzahk@gmail.com>
2021-10-11 04:53:56 -0500

    [UPDATE] Implemented products service and product entity

---
+++ src/services/products.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'bla bla',
+      price: 122,
+      image: '',
+      stock: 12,
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+  create(payload) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+  update(id: number, payload) {
+    const productIndex = this.products.findIndex((item) => item.id === id);
+    this.products[productIndex] = { id, ...payload };
+  }
+  delete(id: number) {
+    const productIndex = this.products.findIndex((item) => item.id === id);
+    this.products.splice(productIndex, 1);
+  }
+}
