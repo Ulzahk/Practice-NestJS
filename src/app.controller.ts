e7323a63663de0d4ae8dc865886cb69c91ba3b1e
Ulzahk <ulzahk@gmail.com>
2021-10-10 19:59:42 -0500

    [UPDATE] Added diffent classes using Single Responsibility Principle

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,27 +17,4 @@ export class AppController {
   hello() {
     return 'with NestJS';
   }
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `products: limit=> ${limit} offset=> ${offset} brand=>${brand}`;
-  }
-  @Get('products/filter')
-  getProductFilter() {
-    return `I\'m a filter`;
-  }
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-  @Get('categories/:categoryId/products/:productId')
-  getCategory(
-    @Param('categoryId') categoryId: string,
-    @Param('productId') productId: string,
-  ) {
-    return `product ${productId} and ${categoryId}`;
-  }
 }
